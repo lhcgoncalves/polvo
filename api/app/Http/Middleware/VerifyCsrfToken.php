25cfbcebfdaf45c7370b91b8b082ca7809332143
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2018-07-24 23:14:15 -0300

    disable cors to api

--- api/app/Http/Middleware/VerifyCsrfToken.php
+++ api/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'products',
-        'orders'
+        'products/*',
+        'orders/*'
     ];
 }
